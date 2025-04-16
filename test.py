dc96fdc6eab5d1961f5bb3580c93ab99e806123a
Muhammad Danish <malikmdanish000@gmail.com>
2025-04-16 19:38:34 +0500

    seperate markdown

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 from markitdown import MarkItDown
 
 md = MarkItDown(enable_plugins=False) # Set to True to enable plugins
-result = md.convert("job_description.pdf")
 
-with open("markdown.md", "w") as f:
+result = md.convert("resume.pdf")
+with open("resume.md", "w") as f:
+    f.write(result.text_content)
+
+result = md.convert("job_description.pdf")
+with open("job_description.md", "w") as f:
     f.write(result.text_content)
